c07febfa0b21341335340769b6145a66dc9706e9
Namburu Rama Sathwik <nambururama17@gmail.com>
2025-06-19 12:06:27 +0530

    Add files via upload

---
+++ Operator.java
@@ -0,0 +1,91 @@
+/* Arthmetic operators
+public class Arithmetic{
+public static void main(String[] args){
+int a=10,b=5;
+System.out.println("Add: "+ (a+b));
+System.out.println("Sub: "+ (a-b));
+System.out.println("Mul: "+ (a*b));
+System.out.println("Div: "+ (a/b));
+}
+}*/
+
+/* Assignment operators
+class Assignment{
+public static void main(String[] args){
+int a=10,b=5;
+a+=b;
+System.out.println(+a);
+a-=b;
+System.out.println(+a);
+a*=b;
+System.out.println(+a);
+a/=b;
+System.out.println(+a);
+a%=b;
+System.out.println(+a);
+}
+}*/
+
+/* Bitwise operators
+class Bitwise{
+public static void main(String[] args)
+{
+int a=10;
+int b=5;
+System.out.println(a & b);
+System.out.println(a | b);
+System.out.println(a ^ b);
+System.out.println(a << b);
+System.out.println(a >> b);
+System.out.println(~b);
+}
+}*/
+
+/* Comparison operators
+class Comparison{
+public static void main(String[] args){
+int a=10,b=5;
+System.out.println(a=b);
+System.out.println(a==b);
+System.out.println(a!=b);
+System.out.println(a>=b);
+System.out.println(a<=b);
+System.out.println(a<b);
+System.out.println(a>b);
+}
+}*/
+
+/* condition operators
+class condition{
+public static void main(String[] args){
+int a=12;
+System.out.print((a%2==0) ? "Even" : "Odd");
+}
+}*/
+
+
+/* logical operators
+class logical{
+public static void main(String[] args){
+int a=10,b=5,c=20;
+System.out.println((a>b)&&(a<c));
+System.out.println((a>b)||(a>c));
+System.out.println(!(a==b));
+}
+}*/
+
+/* unary operators
+class unary{
+public static void main(String[] args){
+int a=5;
+System.out.println(+a);
+a++;
+System.out.println(+a);
+++a;
+System.out.println(+a);
+a--;
+System.out.println(+a);
+--a;
+System.out.println(+a);
+}
+}*/
